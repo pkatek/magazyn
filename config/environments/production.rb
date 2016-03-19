3a68b730144395572d0cf95b729fd720981dbce4
pkatek <patrykkatek@gmail.com>
2016-03-19 21:57:23 +0100

    koniec testu

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,6 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
@@ -26,10 +25,8 @@ Rails.application.configure do
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-   config.assets.js_compressor = :uglifier
-   config.assets.css_compressor = :sass
-  config.assets.initialize_on_precompile = true
-
+  config.assets.js_compressor = :uglifier
+  # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
