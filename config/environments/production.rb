1200649833aff34bf4714358e0609828b2df90d6
pkatek <patrykkatek@gmail.com>
2016-03-19 21:46:35 +0100

    test?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,7 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
@@ -27,6 +28,8 @@ Rails.application.configure do
   # Compress JavaScripts and CSS.
    config.assets.js_compressor = :uglifier
    config.assets.css_compressor = :sass
+  config.assets.initialize_on_precompile = true
+
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
